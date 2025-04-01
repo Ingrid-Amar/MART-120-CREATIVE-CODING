b2cf9a2137e32e80e47667f2a885a969a0e13421
Ingrid (Hannah) Bailey <144834020+Ingrid-Amar@users.noreply.github.com>
2025-03-31 22:07:55 -0600

    Week-9 Update

---
+++ Week-9/sketch.js
@@ -0,0 +1,24 @@
+function setup(){
+    createCanvas(300,500);
+  }
+  function draw() {
+    background(220);
+    rect(142.5,125,15,25);
+    square(160,70,10);
+    rect(170,80,10,50);
+    square(130,70,10);
+    rect(120,80,10,50);
+    ellipse(150,100,50,60);
+    triangle(100, 300,150, 150, 200, 300);
+    square(115,140,70);
+    rect(115,140,15,70);
+    rect(170,140,15,70);
+    rect(130,300,15,70);
+    rect(155,300,15,70);
+    point(140,95);
+    point(160,95);
+    point(150,100);
+    line(140,110,160,110);
+    text("Ingrids Fit!",120,50)
+    text("Ingrid Bailey", 115, 390)
+  }
